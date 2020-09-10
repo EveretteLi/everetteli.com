f55f069ffa85802d3fb98209af6f8424cf10e69b
Everette Li <LYP_Ev@Everette-Macbook.local>
2020-09-09 17:29:48 -0700

    debug

--- js/passVarify.js
+++ js/passVarify.js
@@ -1,8 +1,7 @@
 
-const PASSWORD_HASH = "ZnJhbmtidXR0";
+const PASSWORD_HASH = "ZnJhbmtidXR0";//frankbutt
 
 function varify(ps) {
-    console.log(ps);
     let psw = window.btoa(ps);
     return psw === PASSWORD_HASH;
 }
